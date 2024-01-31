c2eb89cccc7767591d8df093de72f2121203cbb5
ImArthurMendes <arthur.mendes.lucas115@gmail.com>
2024-01-30 21:15:32 -0300

    alteracoes

--- script.js
+++ script.js
@@ -61,3 +61,4 @@ function voltarFaixa (){
 botaoPlayPause.addEventListener('click', tocarOuPausar)
 botaoAvancar.addEventListener('click', proximaFaixa)
 botaoVoltar.addEventListener('click', voltarFaixa)
+
